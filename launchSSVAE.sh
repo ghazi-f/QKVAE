774ad33d52699bd190789806225497c4fd789bb7
ghazy <ghazi.felhi@gmail.com>
2020-05-23 00:44:18 +0200

    -  corrected grad_accumulation with regard to double step Semi-Supervision optimization scheme

--- launchSSVAE.sh
+++ launchSSVAE.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-python train.py --losses "SSVAE" --test_name "SSVAE/0.03" --supervision_proportion 0.03
-python train.py --losses "SSVAE" --test_name "SSVAE/0.1" --supervision_proportion 0.1
-python train.py --losses "SSVAE" --test_name "SSVAE/0.3" --supervision_proportion 0.3
-python train.py --losses "SSVAE" --test_name "SSVAE/1.0" --supervision_proportion 1.0
+python train.py --losses "SSVAE" --test_name "SSVAE/0.03" --supervision_proportion 0.03 --grad_accu 16
+python train.py --losses "SSVAE" --test_name "SSVAE/0.1" --supervision_proportion 0.1 --grad_accu 16
+python train.py --losses "SSVAE" --test_name "SSVAE/0.3" --supervision_proportion 0.3 --grad_accu 16
+python train.py --losses "SSVAE" --test_name "SSVAE/1.0" --supervision_proportion 1.0 --grad_accu 16
