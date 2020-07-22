92bc6567710ac50d55e6dcf0bce4e3e23015855f
ghazy <ghazi.felhi@gmail.com>
2020-07-22 14:18:59 +0200

    - Setup lanches for sentence level semi-supervision on IMDB

--- launchSupervised.sh
+++ launchSupervised.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-python sent_train.py --losses "S" --test_name "IMDB/Supervised/0.03" --supervision_proportion 0.03 --device "gpu:2"
-python sent_train.py --losses "S" --test_name "IMDB/Supervised/0.1" --supervision_proportion 0.1 --device "gpu:2"
-python sent_train.py --losses "S" --test_name "IMDB/Supervised/0.3" --supervision_proportion 0.3 --device "gpu:2"
-python sent_train.py --losses "S" --test_name "IMDB/Supervised/1.0" --supervision_proportion 1.0 --device "gpu:2"
+python sent_train.py --losses "S" --test_name "IMDB/Supervised/0.03" --supervision_proportion 0.03 --device "cuda:2"
+python sent_train.py --losses "S" --test_name "IMDB/Supervised/0.1" --supervision_proportion 0.1 --device "cuda:2"
+python sent_train.py --losses "S" --test_name "IMDB/Supervised/0.3" --supervision_proportion 0.3 --device "cuda:2"
+python sent_train.py --losses "S" --test_name "IMDB/Supervised/1.0" --supervision_proportion 1.0 --device "cuda:2"
