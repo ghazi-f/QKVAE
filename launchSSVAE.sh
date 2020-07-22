2fe1bda7535bbbd9941a3b607e224cc5b8e61136
ghazy <ghazi.felhi@gmail.com>
2020-07-22 19:02:58 +0200

    - Setup launches for sentence level semi-supervision on IMDB

--- launchSSVAE.sh
+++ launchSSVAE.sh
@@ -3,7 +3,3 @@ python sent_train.py --losses "SSVAE" --test_name "IMDB/SSVAE/0.03" --supervisio
 python sent_train.py --losses "SSVAE" --test_name "IMDB/SSVAE/0.1" --supervision_proportion 0.1
 python sent_train.py --losses "SSVAE" --test_name "IMDB/SSVAE/0.3" --supervision_proportion 0.3
 python sent_train.py --losses "SSVAE" --test_name "IMDB/SSVAE/1.0" --supervision_proportion 1.0
-python sent_train.py --losses "SSVAE" --test_name "IMDB/SSVAE/0.03w" --supervision_proportion 0.03 --generation_weight 0.001
-python sent_train.py --losses "SSVAE" --test_name "IMDB/SSVAE/0.1w" --supervision_proportion 0.1 --generation_weight 0.001
-python sent_train.py --losses "SSVAE" --test_name "IMDB/SSVAE/0.3w" --supervision_proportion 0.3 --generation_weight 0.001
-python sent_train.py --losses "SSVAE" --test_name "IMDB/SSVAE/1.0w" --supervision_proportion 1.0 --generation_weight 0.001
