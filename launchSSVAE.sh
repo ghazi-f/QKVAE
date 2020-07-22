7e53ef6a6f017af4119eb3f0258ea39636f394b8
ghazy <ghazi.felhi@gmail.com>
2020-07-22 14:12:11 +0200

    - Setup lanches for sentence level semi-supervision on IMDB

--- launchSSVAE.sh
+++ launchSSVAE.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-python train.py --losses "SSVAE" --test_name "IMDB/SSVAE/0.03" --supervision_proportion 0.03
-python train.py --losses "SSVAE" --test_name "IMDB/SSVAE/0.1" --supervision_proportion 0.1
-python train.py --losses "SSVAE" --test_name "IMDB/SSVAE/0.3" --supervision_proportion 0.3
-python train.py --losses "SSVAE" --test_name "IMDB/SSVAE/1.0" --supervision_proportion 1.0
-python train.py --losses "SSVAE" --test_name "IMDB/SSVAE/0.03w" --supervision_proportion 0.03 --generation_weight 0.001
-python train.py --losses "SSVAE" --test_name "IMDB/SSVAE/0.1w" --supervision_proportion 0.1 --generation_weight 0.001
-python train.py --losses "SSVAE" --test_name "IMDB/SSVAE/0.3w" --supervision_proportion 0.3 --generation_weight 0.001
-python train.py --losses "SSVAE" --test_name "IMDB/SSVAE/1.0w" --supervision_proportion 1.0 --generation_weight 0.001
+python sent_train.py --losses "SSVAE" --test_name "IMDB/SSVAE/0.03" --supervision_proportion 0.03
+python sent_train.py --losses "SSVAE" --test_name "IMDB/SSVAE/0.1" --supervision_proportion 0.1
+python sent_train.py --losses "SSVAE" --test_name "IMDB/SSVAE/0.3" --supervision_proportion 0.3
+python sent_train.py --losses "SSVAE" --test_name "IMDB/SSVAE/1.0" --supervision_proportion 1.0
+python sent_train.py --losses "SSVAE" --test_name "IMDB/SSVAE/0.03w" --supervision_proportion 0.03 --generation_weight 0.001
+python sent_train.py --losses "SSVAE" --test_name "IMDB/SSVAE/0.1w" --supervision_proportion 0.1 --generation_weight 0.001
+python sent_train.py --losses "SSVAE" --test_name "IMDB/SSVAE/0.3w" --supervision_proportion 0.3 --generation_weight 0.001
+python sent_train.py --losses "SSVAE" --test_name "IMDB/SSVAE/1.0w" --supervision_proportion 1.0 --generation_weight 0.001
--- launchSupervised.sh
+++ launchSupervised.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-python train.py --losses "S" --test_name "IMDB/Supervised/0.03" --supervision_proportion 0.03
-python train.py --losses "S" --test_name "IMDB/Supervised/0.1" --supervision_proportion 0.1
-python train.py --losses "S" --test_name "IMDB/Supervised/0.3" --supervision_proportion 0.3
-python train.py --losses "S" --test_name "IMDB/Supervised/1.0" --supervision_proportion 1.0
+python sent_train.py --losses "S" --test_name "IMDB/Supervised/0.03" --supervision_proportion 0.03
+python sent_train.py --losses "S" --test_name "IMDB/Supervised/0.1" --supervision_proportion 0.1
+python sent_train.py --losses "S" --test_name "IMDB/Supervised/0.3" --supervision_proportion 0.3
+python sent_train.py --losses "S" --test_name "IMDB/Supervised/1.0" --supervision_proportion 1.0
