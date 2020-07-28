be657029047638305f3073618eca21a0bb486d24
ghazy <ghazi.felhi@gmail.com>
2020-07-28 09:31:40 +0200

    - prepared SSiPIWO big launches

---
+++ launchSSiPIWO.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+python sent_train.py --losses "SSiPIWO" --test_name "IMDB/SSiPIWO/0.001" --supervision_proportion 0.001 --grad_accu 8 --batch_size 8 --testing_iw_samples 10 --device "cuda:2"
+python sent_train.py --losses "SSiPIWO" --test_name "IMDB/SSiPIWO/0.003" --supervision_proportion 0.003 --grad_accu 8 --batch_size 8 --testing_iw_samples 10 --device "cuda:2"
+python sent_train.py --losses "SSiPIWO" --test_name "IMDB/SSiPIWO/0.01" --supervision_proportion 0.01 --grad_accu 8 --batch_size 8 --testing_iw_samples 10 --device "cuda:2"
+python sent_train.py --losses "SSiPIWO" --test_name "IMDB/SSiPIWO/0.03" --supervision_proportion 0.03 --grad_accu 8 --batch_size 8 --testing_iw_samples 10 --device "cuda:2"
+python sent_train.py --losses "SSiPIWO" --test_name "IMDB/SSiPIWO/0.1" --supervision_proportion 0.1 --grad_accu 8 --batch_size 8 --testing_iw_samples 10 --device "cuda:2"
+python sent_train.py --losses "SSiPIWO" --test_name "IMDB/SSiPIWO/0.3" --supervision_proportion 0.3 --grad_accu 8 --batch_size 8 --testing_iw_samples 10 --device "cuda:2"
+python sent_train.py --losses "SSiPIWO" --test_name "IMDB/SSiPIWO/1.0" --supervision_proportion 1.0 --grad_accu 8 --batch_size 8 --testing_iw_samples 10 --device "cuda:2"
