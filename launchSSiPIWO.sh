0d70105f3f6e19a357a1185e35050659ef84ca00
ghazy <ghazi.felhi@gmail.com>
2020-07-30 15:46:58 +0200

    - Prepared SSiPIWO small launch

--- launchSSiPIWO.sh
+++ launchSSiPIWO.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-python sent_train.py --losses "SSiPIWO" --test_name "IMDB/SSiPIWO/0.001" --supervision_proportion 0.001 --grad_accu 8 --batch_size 8 --testing_iw_samples 10 --device "cuda:2"
-python sent_train.py --losses "SSiPIWO" --test_name "IMDB/SSiPIWO/0.003" --supervision_proportion 0.003 --grad_accu 8 --batch_size 8 --testing_iw_samples 10 --device "cuda:2"
-python sent_train.py --losses "SSiPIWO" --test_name "IMDB/SSiPIWO/0.01" --supervision_proportion 0.01 --grad_accu 8 --batch_size 8 --testing_iw_samples 10 --device "cuda:2"
-python sent_train.py --losses "SSiPIWO" --test_name "IMDB/SSiPIWO/0.03" --supervision_proportion 0.03 --grad_accu 8 --batch_size 8 --testing_iw_samples 10 --device "cuda:2"
-python sent_train.py --losses "SSiPIWO" --test_name "IMDB/SSiPIWO/0.1" --supervision_proportion 0.1 --grad_accu 8 --batch_size 8 --testing_iw_samples 10 --device "cuda:2"
-python sent_train.py --losses "SSiPIWO" --test_name "IMDB/SSiPIWO/0.3" --supervision_proportion 0.3 --grad_accu 8 --batch_size 8 --testing_iw_samples 10 --device "cuda:2"
-python sent_train.py --losses "SSiPIWO" --test_name "IMDB/SSiPIWO/1.0" --supervision_proportion 1.0 --grad_accu 8 --batch_size 8 --testing_iw_samples 10 --device "cuda:2"
+python sent_train.py --losses "SSiPIWO" --test_name "IMDB/SSiPIWO/0.001small" --supervision_proportion 0.001 ---testing_iw_samples 10 --device "cuda:2" --embedding_dim 200 --z_size 100 --text_rep_h 200 --encoder_h 200 --decoder_h 200 --pos_embedding_dim 100 --pos_h 100
+python sent_train.py --losses "SSiPIWO" --test_name "IMDB/SSiPIWO/0.003small" --supervision_proportion 0.003 --testing_iw_samples 10 --device "cuda:2" --embedding_dim 200 --z_size 100 --text_rep_h 200 --encoder_h 200 --decoder_h 200 --pos_embedding_dim 100 --pos_h 100
+python sent_train.py --losses "SSiPIWO" --test_name "IMDB/SSiPIWO/0.01small" --supervision_proportion 0.01 --testing_iw_samples 10 --device "cuda:2" --embedding_dim 200 --z_size 100 --text_rep_h 200 --encoder_h 200 --decoder_h 200 --pos_embedding_dim 100 --pos_h 100
+python sent_train.py --losses "SSiPIWO" --test_name "IMDB/SSiPIWO/0.03small" --supervision_proportion 0.03 --testing_iw_samples 10 --device "cuda:2" --embedding_dim 200 --z_size 100 --text_rep_h 200 --encoder_h 200 --decoder_h 200 --pos_embedding_dim 100 --pos_h 100
+python sent_train.py --losses "SSiPIWO" --test_name "IMDB/SSiPIWO/0.1small" --supervision_proportion 0.1 --testing_iw_samples 10 --device "cuda:2" --embedding_dim 200 --z_size 100 --text_rep_h 200 --encoder_h 200 --decoder_h 200 --pos_embedding_dim 100 --pos_h 100
+python sent_train.py --losses "SSiPIWO" --test_name "IMDB/SSiPIWO/0.3small" --supervision_proportion 0.3 --testing_iw_samples 10 --device "cuda:2" --embedding_dim 200 --z_size 100 --text_rep_h 200 --encoder_h 200 --decoder_h 200 --pos_embedding_dim 100 --pos_h 100
+python sent_train.py --losses "SSiPIWO" --test_name "IMDB/SSiPIWO/1.0small" --supervision_proportion 1.0 --testing_iw_samples 10 --device "cuda:2" --embedding_dim 200 --z_size 100 --text_rep_h 200 --encoder_h 200 --decoder_h 200 --pos_embedding_dim 100 --pos_h 100
