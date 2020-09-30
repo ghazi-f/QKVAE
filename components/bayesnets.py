- Corrected the way importance weighting is forced for the perplexity measure

components/bayesnets.py
@@ -54,7 +54,7 @@ class BayesNet(nn.Module):
             self.dp_lvl = {lv: 0 for lv in self.variables}
 
     def _get_max_iw_path(self, lv, lvl, force_lv=None):
-        if lv.iw or (force_lv and lv.name in force_lv):
+        if (lv.iw and force_lv is None) or (force_lv and lv.name in force_lv):
             lvl += 1
         if lv not in self.parent:
             return lvl
@@ -127,7 +127,7 @@ class BayesNet(nn.Module):
                     gt_lv = self.variables_star[lv] if lv in self.variables_star else None
 
                     # Repeating inputs if the latent variable is importance weighted
-                    if (lv.iw or (force_iw and lv.name in force_iw)) and n_iw is not None:
+                    if ((lv.iw and force_iw is None) or (force_iw and lv.name in force_iw)) and n_iw is not None:
                         for k, v in lv_conditions.items():
                             expand_arg = [n_iw]+list(v.shape)
                             lv_conditions[k] = v.unsqueeze(0).expand(expand_arg)

sentence_classification/models.py
@@ -310,7 +310,8 @@ class SSSentenceClassification(nn.Module, metaclass=abc.ABCMeta):
             neg_log_perplexity_lb = 0
             total_samples = 0
             infer_prev, gen_prev = None, None
-            force_iw = [v.name for v in self.infer_bn.variables if v.name in [v.name for v in self.gen_bn.variables]]
+            force_iw = [v.name for v in self.infer_bn.variables if (v.name in [v.name for v in self.gen_bn.variables]
+                                                                    and v in self.infer_bn.parent)]
             iwlbo = IWLBo(self, 1)
             for i, batch in enumerate(tqdm(iterator, desc="Getting Model Perplexity")):
                 if batch.text.shape[1] < 2: continue