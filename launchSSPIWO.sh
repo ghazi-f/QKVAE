a0fe4fd9f7f55a0323561e887f9af670f3169104
ghazy <ghazi.felhi@gmail.com>
2020-10-14 12:10:05 +0200

    - Did some cleaning

--- launchSSPIWO.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-python sent_train.py --losses "SSPIWO" --test_name "IMDB/SSPIWO/0.001" --supervision_proportion 0.001 --grad_accu 8 --batch_size 8 --testing_iw_samples 10 --device "cuda:1"
-python sent_train.py --losses "SSPIWO" --test_name "IMDB/SSPIWO/0.003" --supervision_proportion 0.003 --grad_accu 8 --batch_size 8 --testing_iw_samples 10 --device "cuda:1"
-python sent_train.py --losses "SSPIWO" --test_name "IMDB/SSPIWO/0.01" --supervision_proportion 0.01 --grad_accu 8 --batch_size 8 --testing_iw_samples 10 --device "cuda:1"
-#python sent_train.py --losses "SSPIWO" --test_name "IMDB/SSPIWO/0.03" --supervision_proportion 0.03 --grad_accu 8 --batch_size 8 --testing_iw_samples 10 --device "cuda:1"
-#python sent_train.py --losses "SSPIWO" --test_name "IMDB/SSPIWO/0.1" --supervision_proportion 0.1 --grad_accu 8 --batch_size 8 --testing_iw_samples 10 --device "cuda:1"
-#python sent_train.py --losses "SSPIWO" --test_name "IMDB/SSPIWO/0.3" --supervision_proportion 0.3 --grad_accu 8 --batch_size 8 --testing_iw_samples 10 --device "cuda:1"
-#python sent_train.py --losses "SSPIWO" --test_name "IMDB/SSPIWO/1.0" --supervision_proportion 1.0 --grad_accu 8 --batch_size 8 --testing_iw_samples 10 --device "cuda:1"
--- launchnli.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-python nli_train.py --test_name "nlilm/kl1" --kl_th 0.001953125 --device "cuda:2"
-python nli_train.py --test_name "nlilm/kl2" --kl_th 0.00390625 --device "cuda:2"
-python nli_train.py --test_name "nlilm/kl3" --kl_th 0.005859375 --device "cuda:2"
-python nli_train.py --test_name "nlilm/kl4" --kl_th 0.0078125 --device "cuda:2"
