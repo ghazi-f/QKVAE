b479fc2304da85e221d75c93b02761d48a771588
ghazy <ghazi.felhi@gmail.com>
2021-05-18 19:29:00 +0200

    - Launching QKV Transformer for SNLI

--- qkv_train.py
+++ qkv_train.py
@@ -19,7 +19,7 @@ from torch.nn import MultiheadAttention
 k, kz, klstm = 1, 8, 2
 parser.add_argument("--test_name", default='unnamed', type=str)
 parser.add_argument("--data", default='nli', choices=["nli", "ontonotes", "yelp"], type=str)
-parser.add_argument("--csv_out", default='disentFinal.csv', type=str)
+parser.add_argument("--csv_out", default='disentqkv.csv', type=str)
 parser.add_argument("--max_len", default=17, type=int)
 parser.add_argument("--batch_size", default=128, type=int)
 parser.add_argument("--grad_accu", default=1, type=int)
