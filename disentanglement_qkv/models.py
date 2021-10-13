- Corrected variable counting for structure based normalization

disentanglement_qkv/models.py
@@ -998,7 +998,8 @@ class DisentanglementTransformerVAE(nn.Module, metaclass=abc.ABCMeta):
         var_wise_scores.set_axis([a.split('_')[0] for a in var_wise_scores.axes[1]], axis=1, inplace=True)
         # renormalizing
         struct_array = np.array(var_wise_scores_struct)
-        struct_array = 1-np.concatenate([struct_array, np.zeros((sum(self.h_params.n_latents), 2))], axis=1)
+        n_vars = sum(self.h_params.n_latents) + (1 if "zs" in self.gen_bn.name_to_v else 0)
+        struct_array = 1-np.concatenate([struct_array, np.zeros((n_vars, 2))], axis=1)
         var_wise_scores = var_wise_scores/struct_array
 
         disent_score = 0