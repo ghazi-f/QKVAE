ab3b468096437f3ddafc2e639a907c44d97fb081
ghazy <ghazi.felhi@gmail.com>
2021-10-19 18:13:19 +0200

    - Added an ADVAE BART Graph

--- disentanglement_qkv/graphs.py
+++ disentanglement_qkv/graphs.py
@@ -413,6 +413,58 @@ def get_hqkv_graph_old(h_params, word_embeddings):
 
 # ============================ QKV BART GRAPHS =========================================================================
 
+def get_BARTADVAE(h_params, word_embeddings):
+    xin_size, zin_size = h_params.embedding_dim, h_params.z_size
+    xout_size, zout_size = h_params.vocab_size, h_params.z_size
+    z_repnet = None
+    n_lvls = len(h_params.n_latents)
+    lv_size_props = [lv_n/max(h_params.n_latents) for lv_n in h_params.n_latents]
+    z_sizes = [int(zin_size*lv_size_prop) for lv_size_prop in lv_size_props]
+    # Generation
+    x_gen, xprev_gen = XGen(h_params, word_embeddings), XPrevGen(h_params, word_embeddings, has_rep=False)
+    z_gens = [ZGeni(h_params, z_repnet, i, allow_prior=(i == 0)) for i in range(n_lvls)]
+    zs_xprev_to_x = ConditionalCoattentiveBARTTransformerLink(xin_size, zout_size, xout_size,h_params.decoder_l,
+                                                              Categorical.parameter_activations, word_embeddings,
+                                                              highway=h_params.highway, sbn=None,
+                                                              dropout=h_params.dropout, n_mems=sum(h_params.n_latents),
+                                                              memory=[z.name for z in z_gens], targets=['x_prev'],
+                                                              bidirectional=False,
+                                                              mem_size=int(z_sizes[0]/h_params.n_latents[0]))
+    z_prior = [ConditionalCoattentiveBARTTransformerLink(z_sizes[i], int(h_params.decoder_h*lv_size_props[i+1]),
+                                                         z_sizes[i+1], h_params.decoder_l,
+                                          Gaussian.parameter_activations,
+                                          memory=[z_gens[i].name],
+                                          n_mems=h_params.n_latents[i], mem_size=int(z_sizes[i]/h_params.n_latents[i]),
+                                          highway=h_params.highway, dropout=h_params.dropout)
+               for i in range(n_lvls-1)]
+    # Sharing BART layers
+    for p in z_prior:
+        p.transformer = zs_xprev_to_x.transformer
+
+    number_parameters  = sum(p.numel() for p in zs_xprev_to_x.parameters() if p.requires_grad)
+    print("reconstruction net size:", "{0:05.2f} M".format(number_parameters/1e6))
+    print("prior net sizes:")
+    for i in range(len(z_prior)):
+        number_parameters = sum(p.numel() for p in z_prior[i].parameters() if p.requires_grad)
+        print("{0:05.2f} M".format(number_parameters/1e6))
+
+    # Inference
+    x_inf, z_infs = XInfer(h_params, word_embeddings, has_rep=False), [ZInferi(h_params, z_repnet, i) for i in
+                                                                       range(n_lvls)]
+    z_posterior = [CoattentiveBARTTransformerLink(xin_size, int(lv_size_props[i]*h_params.encoder_h),
+                                                  z_sizes[i], h_params.encoder_l, Gaussian.parameter_activations,
+                                                  n_mems=sum(h_params.n_latents[i+1:n_lvls]) or None,
+                                                  dropout=h_params.dropout,
+                                                  n_targets=h_params.n_latents[i]) for i in range(n_lvls)]
+    infer_edges = [nn.ModuleList([x_inf, z_posti, z_infi]) for z_posti, z_infi in zip(z_posterior, z_infs)]
+    # for retrocompatibility:
+    infer_edges = [infer_edges[0]]+infer_edges[n_lvls:]+infer_edges[1:n_lvls]
+    gen_edges = [nn.ModuleList([z_gens[i], z_prior[i], z_gens[i+1]]) for i in range(n_lvls-1)]+\
+                [nn.ModuleList([var, zs_xprev_to_x, x_gen]) for var in z_gens+[xprev_gen]]
+
+    return {'infer': nn.ModuleList(infer_edges), 'gen':   nn.ModuleList(gen_edges)}, None, x_gen
+
+
 def get_qkv_graphBART(h_params, word_embeddings):
     # This a version of the network where the hparams to dimensions configuration is reworked
     xin_size, zin_size = h_params.embedding_dim, h_params.z_size
